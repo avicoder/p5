6b75ddd47136de682dd709844e9ce6916a424e37
vjex <avinash.mankoo@gmail.com>
2017-10-25 14:46:23 +0530

    Learnt How to use variables

---
+++ Paint/sketch.js
@@ -0,0 +1,19 @@
+function setup(){
+    
+    createCanvas(400,400);
+    background('yellow');
+}
+
+function draw(){
+    
+    noStroke();
+    fill(0,0,0,50);
+    ellipse(mouseX,mouseY,10,10)
+    
+}
+
+function mousePressed(){
+background('yellow');
+    
+    
+}
